333482abd16f397b3b5761f6264e4d32b1a9611f
bio-ton <popov_a_v@mail.ru>
2021-12-03 16:51:38 +0400

    new solution

--- 023. Merge k Sorted Lists.py
+++ 023. Merge k Sorted Lists.py
@@ -1,4 +1,45 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def on_screen(self):
+        def rec(obj):
+            if obj is not None:
+                print(obj.val)
+                rec(obj.next)
+        rec(self)
+
+
 class Solution:
+    def mergeKLists(self, lists) -> ListNode:
+        head = None
+        next = None
+        while True:
+            min_node = None
+            for i, node in enumerate(lists):
+                if node is not None:
+                    if min_node is None:
+                        min_node = node
+                        min_index = i
+                    if node.val < min_node.val:
+                        min_node = node
+                        min_index = i
+            if min_node is None:
+                return head
+            if head is None:
+                head = min_node
+                next = head
+                lists[min_index] = lists[min_index].next
+            else:
+                next.next = min_node
+                next = next.next
+                lists[min_index] = lists[min_index].next
+
+
+
+class Solution2:
     def mergeKLists(self, lists) -> ListNode:
         self.vals = list()
         for li in lists:
@@ -14,3 +55,20 @@ class Solution:
             point.next = ListNode(val)
             point = point.next
         return head.next
+
+lst = [0] * 3
+lst[0] = ListNode(1)
+lst[0].next = ListNode(4)
+lst[0].next.next = ListNode(5)
+
+lst[1] = ListNode(1)
+lst[1].next = ListNode(3)
+lst[1].next.next = ListNode(4)
+
+lst[2] = ListNode(2)
+lst[2].next = ListNode(6)
+
+c = Solution()
+ans = c.mergeKLists(lst)
+ans.on_screen()
+
--- 039. Combination Sum.py
+++ 039. Combination Sum.py
@@ -1,7 +1,25 @@
 from typing import List
 
-
 class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = list()
+        def RS(cur, total, i):
+            nonlocal ans
+            if i >= len(candidates) or total > target:
+                return []
+            if total == target:
+                ans.append(cur.copy())
+                return
+            for j in range(i, len(candidates)):
+                cur.append(candidates[j])
+                RS(cur, total + candidates[j], j)
+                cur.pop()
+
+        RS([], 0, 0)
+        return ans
+
+
+class Solution2:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = list()
 
@@ -23,4 +41,7 @@ class Solution:
 s = Solution()
 candidates = [2, 3, 5]
 target = 18
+candidates = [2, 3, 6, 7]
+target = 7
+
 print(s.combinationSum(candidates, target))
